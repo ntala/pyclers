82b80da9fb35f9cc61275cf15158ce4635cc28cb
ntala <nicolastala79@yahoo.fr>
2018-01-14 11:46:56 +0100

    separating actions between distincts functions

--- src/lecteur.py
+++ src/lecteur.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 
 def est_quadrilatere(contour):
@@ -5,18 +6,26 @@ def est_quadrilatere(contour):
     approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
     res = len(approx) == 4
     return res
-    
-cap = cv2.VideoCapture(1)                
-while(True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+def get_contours_topologie(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     mystere, threshold = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    return (contours, hierarchy)
+
+def redessine_quadrilateres(image):
+    contours, hierarchy = get_contours_topologie(image)
     for contour in contours : 
         if est_quadrilatere(contour) :
-            cv2.drawContours(frame, contour,-1,(0,0,2555),2)
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+            cv2.drawContours(image, contour,-1,(0,0,2555),2)
+    
+if __name__ == '__main__' :
+    cap = cv2.VideoCapture(0)                
+    while(True):
+        ret, frame = cap.read()
+        redessine_quadrilateres(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
