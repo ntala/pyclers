504c28a5de04b1091a2e32697328e6f17bbeb636
ntala <nicolastala79@yahoo.fr>
2018-01-23 20:53:56 +0100

    improve detection with canny edge detector
    use the canny edge detector algorithm to detect contour and get rid of the quadrangle test that failed to detect  many signs.

--- src/lecteur.py
+++ src/lecteur.py
@@ -2,56 +2,50 @@
 import cv2
 import numpy as np
 
-def est_quadrilatere(contour):
-    peri = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
-    res = len(approx) == 4 and peri > 10
-    return res
-
 def get_contours_topologie(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    mystere, threshold = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    blurred = cv2.blur(gray,(3,3))
+    ced_image = cv2.Canny(blurred,100,180)
+    cv2.imshow('canny', ced_image)
+    contours, hierarchy = cv2.findContours(ced_image, cv2.RETR_TREE,
+                                            cv2.CHAIN_APPROX_NONE)
     return (contours, hierarchy)
-
-def redessine_quadrilateres(image):
-    contours, hierarchy = get_contours_topologie(image)
-    for contour in contours : 
-        if est_quadrilatere(contour) :
-            cv2.drawContours(image, contour,-1,(0,0,2555),2)
     
 def redessine_quadrilateres_et_peres(image):
     contours, hierarchy = get_contours_topologie(image)
-    print hierarchy[0]
-    for rang, contour in enumerate(contours) : 
-        if est_quadrilatere(contour) :
-            cv2.drawContours(image, contour,-1,(0,0,2555),2)
-            print rang
-            #print contours[rang]
-            print hierarchy[0][rang]
-            try :
-                rang_pere = hierarchy[0][rang][3]
-                if rang_pere != -1 :
-                    pere = contours[rang_pere]
-                    cv2.drawContours(image,pere,-1,(0,255,0),2)
-            except :
-                pass
+    for rang, contour in enumerate(contours) :
+        topologie = hierarchy[0][rang]
+        if topologie[0] == -1 and topologie[1] == -1 \
+        and topologie[2] == -1 and topologie[3] != -1:
+            rang_pere = topologie[3]
+            pere = contours[rang_pere]
+            topologie_pere = hierarchy[0][rang_pere]
+            rang_grand_pere = topologie_pere[3]
+            if rang_grand_pere != -1 :
+                grand_pere = contours[rang_grand_pere] 
+                cv2.drawContours(image, contour,-1,(0,0,2555),2)
+                cv2.drawContours(image,grand_pere,-1,(0,255,0),2)
             
 if __name__ == '__main__' :
+    image = cv2.imread('img/plickerexample.jpg')
+    cv2.imwrite('img/test2/capture.jpg', image)
+    redessine_quadrilateres_et_peres(image)
+    cv2.imshow('example',image)
+    cv2.imwrite('img/test2/capture_contours.jpg', image)
+    cv2.waitKey(0)
+    
     cap = cv2.VideoCapture(0)
-    #image = cv2.imread('img/output.jpg')
-    #print image
-    #redessine_quadrilateres_et_peres(image)
-    #cv2.imshow('example',image)
-    #cv2.waitKey(0)
     while(True):
-        ret, frame = cap.read()
+        retour, frame = cap.read()
         frame2 = frame.copy()
+        ced_image = cv2.Canny(frame,100,180)
         redessine_quadrilateres_et_peres(frame)
-        cv2.imshow('frame',frame)
+        cv2.imshow('canny',ced_image)
+        cv2.imshow('contours',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite('img/capture.jpg', frame2)
-            cv2.imwrite('img/capture_contours.jpg', frame)
+            #cv2.imwrite('img/capture.jpg', frame2)
+            #cv2.imwrite('img/capture_contours.jpg', frame)
+            #cv2.imwrite('img/capture_canny.jpg', ced_image)
             break
     cap.release()
     cv2.destroyAllWindows()
