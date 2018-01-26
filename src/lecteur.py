4c733d08de5690fa2e6713e9b897222abf54643f
ntala <nicolastala79@yahoo.fr>
2018-01-26 22:16:44 +0100

    change the first vertice in marque_sommets

--- src/lecteur.py
+++ src/lecteur.py
@@ -15,10 +15,26 @@ def get_contours_topologie(image):
 
 def encadrement_motif(contour):
     rect = cv2.minAreaRect(contour)
+    print rect
     box = cv2.cv.BoxPoints(rect) # cv2.boxPoints(rect) for OpenCV 3.x
     box = np.int0(box)
+    print box
     return [box]
 
+def marque_sommet(coordonnees,image,couleur):
+    cv2.circle(image, coordonnees, 3, couleur,-1)
+    
+def marque_sommets(box,image) :
+    couleurs = [(0,0,0),(255,0,0),(0,0,255),(0,255,255)]
+    sommets = [tuple(sommet) for sommet in box[0]]
+    # le premier sommet doit-être situé en bas à gauche du rectangle
+    if sommets[3][1] < sommets[1][1] :
+        sommets = (sommets[1], sommets[2], sommets[3], sommets[0])
+    for couleur, sommet in zip(couleurs,sommets):
+        marque_sommet(sommet,image,couleur)
+    
+    
+    
 def encadre_motifs(image):
     contours, hierarchy = get_contours_topologie(image)
     for rang, contour in enumerate(contours) :
@@ -39,6 +55,7 @@ def encadre_motifs(image):
                 rectangle = encadrement_motif(grand_pere)
                 cv2.drawContours(image, encadrement_motif(grand_pere),
                                  -1,(0,255,0), 2)
+                marque_sommets(rectangle,image)
             
 if __name__ == '__main__' :
     image = cv2.imread('img/plickerexample.jpg')
@@ -46,6 +63,11 @@ if __name__ == '__main__' :
     encadre_motifs(image)
     cv2.imshow('example',image)
     cv2.imwrite('img/test2/capture_contours.jpg', image)
+    cv2.waitKey(0)
+    
+    image = cv2.imread('img/output.jpg')
+    encadre_motifs(image)
+    cv2.imshow('example',image)
     cv2.waitKey(0)
     
     cap = cv2.VideoCapture(0)
