e75a20f3bf9baa5ba7649c4d9e02d6c8daf5ac37
ntala <nicolastala79@yahoo.fr>
2018-01-27 14:38:29 +0100

    extract and show signs : encadre_et_extrait_motifs
    not suitable for video capture, too many windows

--- src/lecteur.py
+++ src/lecteur.py
@@ -11,7 +11,7 @@ def get_contours_topologie(image):
     #cv2.imshow('canny', ced_image)
     contours, hierarchy = cv2.findContours(ced_image, cv2.RETR_TREE,
                                             cv2.CHAIN_APPROX_NONE)
-    return (contours, hierarchy)
+    return (contours, hierarchy, gray)
 
 def encadrement_motif(contour):
     rect = cv2.minAreaRect(contour)
@@ -32,11 +32,20 @@ def marque_sommets(box,image) :
         sommets = (sommets[1], sommets[2], sommets[3], sommets[0])
     for couleur, sommet in zip(couleurs,sommets):
         marque_sommet(sommet,image,couleur)
-    
-    
+
+def extrait_motif(image, box,nom_fichier) :
+    sommets = [tuple(sommet) for sommet in box[0]]
+    xmin = min(sommets, key = lambda t: t[0])[0]
+    xmax = max(sommets, key = lambda t: t[0])[0]
+    ymin = min(sommets, key = lambda t: t[1])[1]
+    ymax = max(sommets, key = lambda t: t[1])[1]
+    #print [xmin, xmax, ymin, ymax]
+    zone = image[ymin:ymax,xmin:xmax]
+    print zone
+    cv2.imshow(nom_fichier,zone)
     
 def encadre_motifs(image):
-    contours, hierarchy = get_contours_topologie(image)
+    contours, hierarchy, gris = get_contours_topologie(image)
     for rang, contour in enumerate(contours) :
         topologie = hierarchy[0][rang]
         # le test suivant vérifie que le contour ne contient pas
@@ -56,17 +65,40 @@ def encadre_motifs(image):
                 cv2.drawContours(image, encadrement_motif(grand_pere),
                                  -1,(0,255,0), 2)
                 marque_sommets(rectangle,image)
-            
+                
+def encadre_et_extrait_motifs(image):
+    contours, hierarchy, gris = get_contours_topologie(image)
+    for rang, contour in enumerate(contours) :
+        topologie = hierarchy[0][rang]
+        # le test suivant vérifie que le contour ne contient pas
+        # de contour, a un père et est le seul contour contenu 
+        # par son père.
+        if topologie[0] == -1 and topologie[1] == -1 \
+        and topologie[2] == -1 and topologie[3] != -1:
+            rang_pere = topologie[3]
+            pere = contours[rang_pere]
+            topologie_pere = hierarchy[0][rang_pere]
+            # les contours rectangles qui m'intéressent sont 
+            # 'souvent' inclus dans eux-même
+            rang_grand_pere = topologie_pere[3]
+            if rang_grand_pere != -1 :
+                grand_pere = contours[rang_grand_pere]
+                rectangle = encadrement_motif(grand_pere)
+                cv2.drawContours(image, encadrement_motif(grand_pere),
+                                 -1,(0,255,0), 2)
+                marque_sommets(rectangle,image)
+                extrait_motif(gris,rectangle, 'signal'+str(rang))
+                
 if __name__ == '__main__' :
     image = cv2.imread('img/plickerexample.jpg')
     cv2.imwrite('img/test2/capture.jpg', image)
-    encadre_motifs(image)
+    encadre_et_extrait_motifs(image)
     cv2.imshow('example',image)
     cv2.imwrite('img/test2/capture_contours.jpg', image)
     cv2.waitKey(0)
     
     image = cv2.imread('img/output.jpg')
-    encadre_motifs(image)
+    encadre_et_extrait_motifs(image)
     cv2.imshow('example',image)
     cv2.waitKey(0)
     
