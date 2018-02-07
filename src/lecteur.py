3d7a042240542c89f928029045f82a972d4dde37
Romulus <romain_viard@yahoo.fr>
2018-02-07 22:20:35 +0100

    creation de la base de symbole a reconnaitre

--- src/lecteur.py
+++ src/lecteur.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
+import os
 
 def get_contours_topologie(image):
     '''
@@ -62,6 +63,7 @@ def extrait_motif(image, box, nom_fichier) :
     Affiche l'image du panneau redressée et découpée en rectangles 
     significatifs et retourne la "matrice" résumant le panneau.
     """
+    str_code=''
     sommets = [tuple(sommet) for sommet in box[0]]
     # le premier sommet doit-être situé en bas à gauche du rectangle
     if sommets[3][1] < sommets[1][1] :
@@ -73,9 +75,9 @@ def extrait_motif(image, box, nom_fichier) :
     zone = image[ymin:ymax,xmin:xmax]
     depart = np.float32(tuple(map(lambda t: (t[0]-xmin, t[1]-ymin),
                                     sommets[:3])))
-    cible = np.float32(((10,210),(10,10),(210,10)))
+    cible = np.float32(((0,200),(0,0),(200,0)))
     mat = cv2.getAffineTransform(depart,cible)
-    zone_redressee = cv2.warpAffine(zone,mat,(210,210))
+    zone_redressee = cv2.warpAffine(zone,mat,(200,200))
     zone_contrastee = cv2.threshold(zone_redressee,
                                    80,
                                    255,
@@ -90,24 +92,35 @@ def extrait_motif(image, box, nom_fichier) :
             [0,0,0,0,0],
             [0,0,0,0,0],
             [0,0,0,0,0]]
-    for i in xrange(5) :
-        for j in xrange(5) :
-            xmin = 10+40*i+ECART
-            xmax = 10+40*(i+1)-ECART
-            ymin = 10+40*j+ECART
-            ymax = 10+40*(j+1)-ECART
+    for j in xrange(5) :
+        for i in xrange(5) :
+            xmin = 40*i+ECART
+            xmax = 40*(i+1)-ECART
+            ymin = 40*j+ECART
+            ymax = 40*(j+1)-ECART
             zone = zone_contrastee[ymin:ymax,xmin:xmax]
             
             if cv2.mean(zone)[0]>127 :
                 code[j][i] = 1
+                str_code +='0';
+            else:
+                str_code +='1';
             cv2.rectangle(zone_contrastee,
-                          (10+40*i+ECART,10+ECART+40*j),
-                          (10+40*(i+1)-ECART,10+40*(j+1)-ECART),
+                          (40*i+ECART,ECART+40*j),
+                          (40*(i+1)-ECART,40*(j+1)-ECART),
                           (255,255,255))
-    cv2.imshow(nom_fichier,zone_contrastee)
-    return code
+    
+    str_baseCode='1000101110010100111010001' #= 18295249
+    if (int(str_code,2) & int(str_baseCode,2)) == 18295249:
+        int_ret=int(str_code,2)
+    else:
+        int_ret=18295249
+    #cv2.imshow(nom_fichier,zone_contrastee)    
+    return int_ret
+    
 
-def encadre_et_extrait_motifs(image):
+def encadre_et_extrait_motifs(image,visu):
+    res=[]
     contours, hierarchy, gris = get_contours_topologie(image)
     for rang, contour in enumerate(contours) :
         topologie = hierarchy[0][rang]
@@ -125,25 +138,69 @@ def encadre_et_extrait_motifs(image):
             if rang_grand_pere != -1 :
                 grand_pere = contours[rang_grand_pere]
                 rectangle = encadrement_motif(grand_pere)
-                cv2.drawContours(image, rectangle,
-                                 -1,(0,255,0), 2)
-                marque_sommets(rectangle,image)
-                if cv2.contourArea(grand_pere) > 400 :
+                if visu:
+                    cv2.drawContours(image, rectangle,-1,(0,255,0), 2)
+                    marque_sommets(rectangle,image)
+                if cv2.contourArea(grand_pere) > 0 :
                     matrice = extrait_motif(gris,rectangle, 'signal')
-                    print 'matrice'
-                    for l in matrice :
-                        print l
+                    res.append(matrice)
                 #cv2.imshow('signal'+str(rang),image)
-                
+    return res
+
+def build_db(path_folder):
+    student_name=[]
+    laDB=np.array([[0,0,0,0]])
+    listF = os.listdir(path_folder)
+    for ima in listF :
+        image = cv2.imread(path_folder+ima)
+        imaSav = image
+        rows,cols,rgb = image.shape
+        listBoxA=encadre_et_extrait_motifs(image,False)
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+        image = cv2.warpAffine(imaSav,M,(cols,rows))
+        listBoxB=encadre_et_extrait_motifs(image,False)
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
+        image = cv2.warpAffine(imaSav,M,(cols,rows))
+        listBoxC=encadre_et_extrait_motifs(image,False)
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),-90,1)
+        image = cv2.warpAffine(imaSav,M,(cols,rows))
+        listBoxD=encadre_et_extrait_motifs(image,False)
+        data=np.array([[listBoxA[0],listBoxB[0],listBoxC[0],listBoxD[0]]])
+        laDB=np.concatenate((laDB,data), axis=0)
+        student_name.append(ima)
+    #bon je reconnais que faire une ligne avec des 0 et l'effacer à la fin ce n'est pas propre et il y a moyen de faire mieux
+    print(laDB)
+    print(np.size(laDB))
+    np.delete(laDB,0)
+    return student_name,laDB
+
 if __name__ == '__main__' :
+    #creation de la base lecture des images dans le repertoire (un repertoire par classe)
+    #une liste de nom associée à un tableau meme nombre de ligne et quatres colonnes
+    listeName,listeIma=build_db('img/db/')
+    print(listeName)
+    print(listeIma)
     cv2.waitKey(1)
     cap = cv2.VideoCapture(0)
-    while(True):
+    Letter_code=['A','B','C','D']
+    #tant qu'il y a des noms d'eleves dans la liste
+    while(np.size(listeName)>0):
         retour, frame = cap.read()
         frame2 = frame.copy()
         ced_image = cv2.Canny(frame,100,180)
         #encadre_motifs(frame)
-        encadre_et_extrait_motifs(frame)
+        listBox=encadre_et_extrait_motifs(frame,True)
+        for box in enumerate(listBox):
+            print(box)
+            box_index=np.where(listeIma == box) 
+            print(box_index)
+            if np.size(box_index) > 0:
+                #affichage du nom de l'eleve et de la reponse
+                print(listeName[int(box_index[0])])
+                print(Letter_code[int(box_index[1])])
+                #je supprime des deux listes (nom et index) 
+                del listeName[int(box_index[0])]
+                del listeIma[int(box_index[0])]
         #cv2.imshow('canny',ced_image)
         cv2.imshow('contours',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
