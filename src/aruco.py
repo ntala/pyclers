8f0d547b8e258825dcd5249671aad91962698bfb
nicolastala <contact@nicolastala.fr>
2019-05-12 22:29:17 +0200

    lecture des positions des panneaux aruco

--- src/aruco.py
+++ src/aruco.py
@@ -1,33 +1,51 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat May 11 13:15:55 2019
 
-@author: caged2013
-"""
-
-
-# import numpy as np
-import cv2#, PIL
+import cv2  # , PIL
+import numpy as np
 from cv2 import aruco
+
 # import matplotlib.pyplot as plt
 # import matplotlib as mpl
 # import pandas as pd
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
 
-def marque(image, point):
-    x,y = map(int, point)
-    cv2.rectangle(image, (x-20, y-20), (x+20, y+20), (0,0,255))
+# def marque(image, point):
+#     x,y = map(int, point)
+#     cv2.rectangle(image, (x-20, y-20), (x+20, y+20), (0,0,255))
 
-cap = cv2.VideoCapture(0)
+
+def ordonnees_repere(points):
+    points_2 = np.concatenate([points[1:], points[0:1]])
+    res = (points[:,1] + points_2[:,1]) / 2
+    return res  
+
+
+def presents(panneaux_detectes):
+    try :
+        if panneaux_detectes :
+            return True
+        else :
+            return False
+    except :
+        if panneaux_detectes.any():
+            return True
+        else :
+            return False
+
+
+cap = cv2.VideoCapture(1)
 while True :
     retour, frame = cap.read()
     if retour :
-            coins, ids, rejets = aruco.detectMarkers(frame,aruco_dict)
-            if ids :
-                premier_coin = coins[0][0][0]
-                marque(frame, premier_coin)
-                print(premier_coin)
+            listes_coins, num_panneaux, rejets = aruco.detectMarkers(frame,aruco_dict)
+            if presents(num_panneaux):
+                for rang, num in enumerate(num_panneaux) :
+                    # premier_coin = listes_coins[0][rang][0]
+                    # marque(frame, premier_coin)
+                    tableau_ordonnees = ordonnees_repere(listes_coins[0][rang])
+                    ordonnee_max = tableau_ordonnees.max()
+                    print(np.where(tableau_ordonnees==ordonnee_max)[0][0])
             cv2.imshow('capture',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
